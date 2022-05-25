53d583e59be28f49445a2e783445c71e7176b5f8
YunSeok-Choid <dbstjr9898@naver.com>
2022-05-25 21:05:51 +0900

    create project

---
+++ Yungsix_Culculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yungsix_Culculator
+//
+//  Created by 최윤석 on 2022/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yungsix_Culculator/Yungsix_CulculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yungsix_CulculatorApp.swift
+//  Yungsix_Culculator
+//
+//  Created by 최윤석 on 2022/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Yungsix_CulculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
