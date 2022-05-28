cfb98975cc51c45cb27f9a21ce0eb5a55523656d
YunSeok-Choi <dbstjr9898@naver.com>
2022-05-28 16:13:50 +0900

    Add : 결과 값 UI 추가

--- Yungsix_Culculator/ContentView.swift
+++ Yungsix_Culculator/ContentView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var result: Int = 0
+    
     var body: some View {
         VStack{
+            
+            Text(result, format: .number)
+                .padding()
+            
             HStack{
                 NumButton(value: 7)
                 
