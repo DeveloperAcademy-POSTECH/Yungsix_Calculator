bf942b976d42f2e11205755a110bffd30f6b289a
YunSeok-Choi <dbstjr9898@naver.com>
2022-05-28 11:14:15 +0900

    Feat : 숫자버튼 UI 생성

---
+++ Yungsix_Culculator/NumButton.swift
@@ -0,0 +1,35 @@
+//
+//  NumButton.swift
+//  Yungsix_Culculator
+//
+//  Created by 최윤석 on 2022/05/27.
+//
+
+import SwiftUI
+
+struct NumButton: View {
+    var value: Int
+    
+    var body: some View {
+        HStack{
+            Button {
+                    
+            } label: {
+                Text("\(value)")
+                    .padding()
+                    .frame(width: 70, height: 70)
+                    .foregroundColor(.white)
+                    .background(.gray)
+                    .font(.largeTitle)
+                    .clipShape(Circle())
+            }
+
+        }
+    }
+}
+
+struct NumButton_Previews: PreviewProvider {
+    static var previews: some View {
+        NumButton(value: 1)
+    }
+}
