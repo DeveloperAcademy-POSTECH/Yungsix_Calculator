c2a8ed00db287a00332f9d7fb9cd8a343745b7ae
YunSeok-Choi <dbstjr9898@naver.com>
2022-06-09 00:07:04 +0900

    Feat : AC, +, -, % 기능 추가

--- Yungsix_Culculator/CalculatorModel.swift
+++ Yungsix_Culculator/CalculatorModel.swift
@@ -10,47 +10,74 @@ import Foundation
 class CalculatorModel : ObservableObject {
     
     @Published var result = "0"
-    
-    var arr = [String]()
+    let secondResult = ""
     
     var first = ""
     var second = ""
-    var operation = ""
+    var operation: ButtonName?
+    var operCheck = false
     
     func inputNumber(button: ButtonName) {              // 숫자 입력 함수
         
-        if result == "0"{
+        if operation != nil && operCheck {              // 연산자가 입력되었으면
+            operCheck = false
+            first = result
+            result = ""
+        }
+        
+        if result == "0" || result == "" {
             result = button.title
-            arr.append(button.title)
+            return
         }else {
             result += button.title
+            return
         }
         
     }
     
     func inputOperaiton(button: ButtonName) {           // 연산자 입력 함수
-    
-        first = result
-        operation = button.title
         
-        if button == .plus {
-            
-        }else if button == .minus {
-                        
-        }else if button == .div {
-            
-        }else if button == .mul {
-            
-        }else if button == .mark {
-            
-        }else if button == .percent {
-            
+        if button == .percent {
+            result = String(Double(result)! / 100.0)
+            return
         }else if button == .clear {
             result = "0"
-        }else if button == .dot {
+            first = ""
+            second = ""
+            operation = nil
+            return
+        }
+        
+        if first == "" {
+            first = result
+            operCheck = true
+            operation = button
+            return
+        }
+        
+        
+        if operation == .plus {
+            let sum = Double(first)! + Double(result)!
+            operation = button
+            result = String(format: "%.f", sum)
+            operCheck = true
+            
+        }else if operation == .minus {
+            let min = Double(first)! - Double(result)!
+            operation = button
+            result = String(format: "%.f", min)
+            operCheck = true
+            
+        }else if operation == .div {
+            
+        }else if operation == .mul {
+            
+        }else if operation == .mark {
+            
+        }else if operation == .dot {
             
         }else if button == .equal {
-            
+            operation = nil
         }
         
     }
